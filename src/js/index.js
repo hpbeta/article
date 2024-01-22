6e4a4733de15997e082fd3cfebe86b52b8575d91
João Lucas <hpbeta16.l@gmail.com>
2024-01-22 14:05:52 -0300

    adicionei o javascript

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const botaoShare = document.querySelector(".btn-share")
+const share = document.querySelector(".balao")
+
+
+botaoShare.addEventListener("click" , () => {
+    share.classList.toggle("mostrar")
+})
+
+
+
+
+
+
+
